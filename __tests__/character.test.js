deab8d14af96fcd2763a3626aac21cfe4d6808d5
Kyle Crawford <kcraw217@gmail.com>
2023-04-12 10:43:28 -0700

    good fail for test to instantiate new Character object
    Co-authored-by: Sarah Reimann <reimannsarah45@gmail.com>

---
+++ __tests__/character.test.js
@@ -0,0 +1,36 @@
+import Character from './../src/js/character.js';
+
+describe('Character', () => {
+
+  test('should correctly create a character object with many properties', () => {
+    const meanGirl = new Character("Claire", 6, 9, 20, 17, 3, 80, ["sprinkler", "cabbagePatch", "runningMan"]);
+    expect(meanGirl.name).toEqual("Claire");
+    expect(meanGirl.strength).toEqual(6);
+    expect(meanGirl.intellingence).toEqual(9);
+    expect(meanGirl.snark).toEqual(20);
+    expect(meanGirl.charm).toEqual(17);
+    expect(meanGirl.weirdness).toEqual(3);
+    expect(meanGirl.repPoints).toEqual(80);
+  });
+
+  // test('should correctly determine whether three lengths are not a triangle', () => {
+  //   const notTriangle = new Triangle(3,9,22);
+  //   expect(notTriangle.checkType()).toEqual("not a triangle");
+  // });
+
+  // test('should correctly determine whether three lengths make an isosceles triangle', () => {
+  //   const isocTriangle = new Triangle(5,5,7)
+  //   expect(isocTriangle.checkType()).toEqual("isosceles triangle");
+  // });
+
+  // test('should correctly determine whether three lengths make an scalene triangle', () => {
+  //   const scalTriangle = new Triangle(2,3,4)
+  //   expect(scalTriangle.checkType()).toEqual("scalene triangle");
+  // });
+
+  // test('should correctly determine whether three lengths make an equilateral triangle', () => {
+  //   const equiTriangle = new Triangle(5,5,5)
+  //   expect(equiTriangle.checkType()).toEqual("equilateral triangle");
+  // });
+
+});
---
+++ src/js/character.js
@@ -0,0 +1,27 @@
+export default class Character {
+  constructor(name, strength, intelligence, snark, charm, weirdness, repPoints, moves) {
+    // this.name = name;
+    // this.strength = strength;
+    // this.intelligence = intelligence;
+    // this.snark = snark;
+    // this.charm = charm;
+    // this.weirdness = weirdness;
+    // this.repPoints = repPoints;
+    // this.moves = moves; 
+    // this.winPoints = 0;
+    // this.deflectPoints = 0;
+    // this.level = 1;
+  }
+
+  // checkType() {
+  //   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
+  //     return "not a triangle";
+  //   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
+  //     return "scalene triangle";
+  //   }  else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+  //     return "equilateral triangle";
+  //   } else {
+  //     return "isosceles triangle";
+  //   }
+  // }    
+}
