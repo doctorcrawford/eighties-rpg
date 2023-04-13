c16d4e8448bfcaf5d2c4ae4b6e289c42fe4c34bc
Kyle Crawford <kcraw217@gmail.com>
2023-04-13 10:25:04 -0700

    good fail for test to character to take a hit
    co-authored-by: Kyle Crawford <kcraw217@gmail.com>
    co-authored-by: Emma Gerigscott <elgerig@gmail.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -3,9 +3,11 @@ import { DanceMove } from './../src/js/dance-move.js';
 
 describe('Character', () => {
   let claire;
+  let sprinkler;
 
   beforeEach(() => {
     claire = new Character("Claire", 6, 9, 17, 20, 3, 70, ["sprinkler", "cabbagePatch", "runningMan"]);
+    sprinkler = new DanceMove("Sprinkler",60, 8);
   });
 
   test('should correctly create a character object with many properties', () => {
@@ -18,11 +20,16 @@ describe('Character', () => {
     expect(claire.repPoints).toEqual(70);
   });
   
-  test('should adjust the moves battle effectiveness and damage', () => {
-    let sprinkler = new DanceMove("Sprinkler",60, 8);
+  test('should adjust the moves battle effectiveness and damage', () => {  
     claire.makeAMove(sprinkler);
     expect(sprinkler.battleEffectiveness).toEqual(73.8);
     expect(sprinkler.battleDamage).toEqual(8.72);
   });
 
+  test('should ', () => {
+    let andrew = new Character("Andrew",20,3,14,17,1,75,["sprinkler","cabbagePatch","runningMan"]);
+    let battleDmg = claire.makeAMove(sprinkler);
+    andrew.takeAHit(battleDmg);
+    expect(andrew.repPoints).toEqual(66.28);
+  });
 });
--- src/js/character.js
+++ src/js/character.js
@@ -17,13 +17,30 @@ export default class Character {
     let snarkAdjust = this.snark * 0.5;
     let intelAdjust = this.intelligence *.5;
     let charmAdjust = this.charm *.5;
-    console.log(theMove.baseEffectiveness);
-    console.log(snarkAdjust);
+    // console.log(theMove.baseEffectiveness);
+    // console.log(snarkAdjust);
     theMove.battleEffectiveness = theMove.baseEffectiveness * (1+ (.01 * (snarkAdjust + intelAdjust + charmAdjust)));
     let strengthAdjust = this.strength *.5;
     let weirdnessAdjust = this.weirdness *.5;
     theMove.battleDamage = theMove.baseDamage * (1 + (.01 * (strengthAdjust + intelAdjust + weirdnessAdjust)));
-    console.log(theMove);
-    console.log(theMove.battleEffectiveness);
+    // console.log(theMove);
+    // console.log(theMove.battleEffectiveness);
+    let battleMap = new Map (
+      [
+        ["effectiveness", theMove.battleEffectiveness],
+        ["damage", theMove.battleDamage]
+      ]
+    );
+    return battleMap;
+  }
+
+  takeAHit() {
+    // let randomNumber = 1;
+    // // let randomNumber = Math.floor(Math.random()*100);
+    // if (randomNumber <= battleMap.get("effectiveness")){
+    //   this.repPoints -= battleMap.get("damage");
+    // }else {
+    //   return "you missed, you got no rizz!";
+    // }
   }
 }
